a200dc2917164bdf65eceeb6d11e8aa3ba2ea891
ttakkku <k2005118@gmail.com>
2019-06-06 20:02:35 +0900

    Add files via upload

---
+++ test/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer = 1234
+
+        Dim b As Single = 1.04
+
+        Dim c As String = "헿"
+
+        Dim d As Boolean = True
+
+        Dim f As Date = # 12/31/2050 6:30:10 AM#
+
+        Dim g As Object = 123
+
+        Label1.Text = a & " , " & b & " , " & c & " , " & d & " , " & f & " , " & g
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+
+
+    End Sub
+End Class
